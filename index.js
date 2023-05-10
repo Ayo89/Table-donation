d502dec33bc5ca4e2f980b0ffd3064c0cf6f1e6a
ayo <ayopro89@gmail.com>
2023-05-10 14:39:51 +0100

    added function give

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-let $total = document.querySelectorAll('.total')[0];
-console.log($total);
-$total += 25;
-console.log($total)
+const needed = document.querySelectorAll('.total')[0];
+const num = document.querySelectorAll('.input')[0];
+const form = document.querySelectorAll('.donation-form')[0];
+const bar = document.querySelectorAll('.barra')[0];
+let total = 1000;
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+    let amount = num.value;
+    let completed = total - needed.innerHTML;
+    completed += parseInt(amount);
+    let result = total - parseInt(completed);
+    needed.innerHTML = Math.max(result, 0);
+    donationPorcent = Math.min((completed / total) * 100, 100)
+    bar.style.width = `${donationPorcent}%`;
+})
+
